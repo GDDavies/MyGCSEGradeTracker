376902343efa7002c46f1f0548d676e71d2b6e1f
George Davies <duhukboy@hotmail.com>
2016-12-02 17:08:39 +0000

    Add qual form setup

--- MyGCSEGradeTracker/AddQualificationViewController.swift
+++ MyGCSEGradeTracker/AddQualificationViewController.swift
@@ -8,44 +8,142 @@
 
 import UIKit
 
-class AddQualificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class AddQualificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var buttonBtmConstraint: NSLayoutConstraint!
+    
+    var textViewOutput = ["", ""]
+    
+    var componentPercentages = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
-
+    @IBAction func saveQualification(_ sender: Any) {
+        print(textViewOutput)
+        print(componentPercentages)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        let keyboardHeight = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
+        
+        UIView.animate(withDuration: 0.3) {
+            self.buttonBtmConstraint.constant = keyboardHeight;
+        }
+        self.view.layoutIfNeeded();
+        print(keyboardHeight);
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.3) {
+            self.buttonBtmConstraint.constant = 0;
+        }
+        self.view.layoutIfNeeded();
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0 {
+            return "Qualification Details"
+        } else {
+            return "Components"
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        
+        if section == 0 {
+            return 2
+        } else {
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = "Test"
+        var cell: CustomTableViewCell?
         
-        return cell
+        if indexPath.section == 0 {
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! CustomTableViewCell?
+            
+            switch indexPath.row {
+            case 0:
+                cell?.labelOutlet.text = "Qualification Name"
+                cell?.placeholderTextOutlet.placeholder = "e.g. Maths"
+            case 1:
+                cell?.labelOutlet.text = "No. of Components"
+                cell?.placeholderTextOutlet.placeholder = "e.g. 4"
+                cell?.placeholderTextOutlet.keyboardType = UIKeyboardType.decimalPad
+            default:
+                break
+            }
+            cell?.selectionStyle = .none
+            
+            cell?.placeholderTextOutlet.tag = indexPath.row
+            cell?.placeholderTextOutlet.addTarget(self, action: #selector(textViewValueChange), for: .editingChanged)
+        }
+        
+        if indexPath.section == 1 {
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "Cell2") as! CustomTableViewCell?
+            cell?.labelOutlet.text = "Edit Components"
+            cell?.accessoryType = .disclosureIndicator
+        }
+        
+        return cell!
+    }
+    
+    func textViewValueChange(sender: UITextField) {
+        
+        let currentValue = sender.text
+        let textRow = sender.tag
+        
+        textViewOutput.remove(at: textRow)
+        textViewOutput.insert(currentValue!, at: textRow)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell2") as! CustomTableViewCell?
+        cell?.contentView.backgroundColor = .orange
+        
+        switch (indexPath.section, indexPath.row) {
+        case (1, _):
+            print("Cell 3 tapped")
+        default:
+            break
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell2") as! CustomTableViewCell?
+            cell?.contentView.backgroundColor = .white
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowComponents" {
+            let destinationVC = segue.destination as? ComponentTableViewController
+            destinationVC?.numberOfComponents = Int(textViewOutput[1])
+        }
     }
 }
---
+++ MyGCSEGradeTracker/CustomTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewCell.swift
+//  MyGradeTarckerV2
+//
+//  Created by George Davies on 03/11/2016.
+//  Copyright © 2016 George Davies. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var placeholderTextOutlet: UITextField!
+    @IBOutlet weak var labelOutlet: UILabel!
+    
+    var activeTextField = UITextField()
+    
+    var textFieldText = ""
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
